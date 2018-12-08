c22dc4297ce5b00eadcfa72a1c875b8dbc0cddc5
Unknown <duanestorey@gmail.com>
2018-12-08 20:58:27 +0000

    Updates to base plugin

---
+++ base-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Base WordPress Plugin
+Plugin URI: https://github.com/duanestorey/base-plugin
+Description: A skeleton of a minimal WordPress plugin, complete with tooling
+Version: 1.0
+Author: Duane Storey
+Author URI: https://duanestorey.com
+Text Domain: base-plugin
+Domain Path: /lang
+*/
